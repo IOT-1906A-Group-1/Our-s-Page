65a82097ab0c295ab02598dacbdd3acf1ebb5dda
mike Yang <15802994845@163.com>
2022-02-28 10:47:18 +0800

    全局化配置

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$ajax = axios
+app.use(Antd).use(store).use(router).mount('#app')
