70db6b22753a2745bd8ec1a6dff86fe4978e9b01
mike Yang <15802994845@163.com>
2022-02-28 10:42:20 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,78 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import ViewMaster from '../views/ViewMaster.vue'
+import Login from '../views/Login.vue'
+import Workbench from '../views/Workbench.vue'
+import AnnualPlan from '../views/AnnualPlan.vue'
+import HRPage from '../views/HRPage.vue'
+import Employ from '../views/Employ.vue'
+import Leave from '../views/Leave.vue'
+import Quit from '../views/Quit.vue'
+import CreateApply from '../views/CreateApply.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/Leave',
+        name: 'Leave',
+        component: Leave
+    },
+    {
+        path: '/AnnualPlan',
+        name: 'AnnualPlan',
+        component: AnnualPlan
+    },
+    {
+        path: '/HRPage',
+        name: 'HRPage',
+        component: HRPage
+    },
+    {
+        path: '/Employ',
+        name: 'Employ',
+        component: Employ
+    },
+    {
+        path: '/Quit',
+        name: 'Quit',
+        component: Quit
+    },
+    {
+        path: '/ViewMaster',
+        name: 'ViewMaster',
+        component: ViewMaster,
+        children: [
+            {
+                path: '/CreateApply',
+                name: 'CreateApply',
+                component: CreateApply
+            },
+            {
+                path: '/Workbench',
+                name: 'Workbench',
+                component: Workbench
+            },
+        ]
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
